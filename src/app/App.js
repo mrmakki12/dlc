77ca2104995738139722175f7b936a5b19b3d264
Tyreeck M <mrmakki12@users.noreply.github.com>
2022-03-27 15:47:13 -0400

    start react-router-dom setup

--- src/app/App.js
+++ src/app/App.js
@@ -1,12 +1,19 @@
 import React from 'react';
 import './App.css';
+// router
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 // components
 import { Header } from '../Components/Header/Header.js';
 
 function App() {
   return (
     <div className="App">
-      <Header />
+      <BrowserRouter>
+        <Routes>
+          
+        </Routes>
+        <Header />
+      </BrowserRouter>
     </div>
   );
 }

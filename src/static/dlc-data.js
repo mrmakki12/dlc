2e62ae0d927ed1692057221c0165794e9bf45b8f
Tyreeck M <mrmakki12@users.noreply.github.com>
2022-03-27 13:39:04 -0400

    sample data

---
+++ src/static/dlc-data.js
@@ -0,0 +1,110 @@
+// data for fake dlc
+export const dlcs = [
+    {
+        name: 'Hiking Simulator: Washington Winter',
+
+        img: './pictures/washington-winter-woods.jpg',
+
+        description: `Take a trip into the calm woods of Washington State, 
+        with beautiful and peaceful wild life.`,
+
+        price: 9.99
+    },
+
+    {
+        name: 'Hiking Simulator: Mount Kilimanjaro',
+
+        img: './pictures/kilimanjaro.jpg',
+
+        description: `Take a long and arduous trip up the tallest mountain in the 
+        world. Look out for wildlife, you won't be given help by the locals. Lather your 
+        character in sunscreen and pack tons of water. Over 90% of players haven't made it 
+        up and back without dying. Are you up to the task?`,
+
+        price: 20.99
+    },
+    
+    {
+        name: 'Hiking Simulator: The Sahara',
+
+        img: './pictures/shara.jpg',
+
+        description: `Carefully navigate the sand dunes of the Sahara. Pack a ton of water you'll need
+        it, we wouldn't want you to experience any hallucinations! Hah ha ha! Over 90% of players haven't
+        made it to the Oasis in time!`,
+
+        price: 17.99
+    },
+
+    {
+        name: 'Hiking Simulator: The Nevada Nightmare',
+
+        img: './pictures/nevada-road.jpg',
+
+        description: `Your car has broken down, you cellphone is dead and it's National Stay At Home Day, so 
+        no one is going to save you. This wasn't a planned hike, so you have no supplies... Will you make it to 
+        the city in time. Over 95% of players cannot, so are you up for the challenge?`,
+
+        price: 15.99
+    },
+
+    {
+        name: 'Hiking Simulator: The Moon Landing',
+
+        img: './pictures/space.jpg',
+
+        description: `The year is 2050 and space trips and now affordable to 25% of the population. Everything goes
+        to plan, but when you landed and started exploring you lost your way. Zero gravity is very fun, until you realize
+        you only have a couple hours of oxygen left in the tank. Use the stars and you quick wits to find the shuddle before its too late!`,
+
+        price: 23.99
+    },
+
+    {
+        name: 'Hiking Simulator: Frozen',
+
+        img: './pictures/winter-wonderland.jpg',
+
+        description: `You're the stunt double for Elsa in the famous movie "Frozen." A scene goes wrong, what was supposed to 
+        be a routine ice slide using your powers turned into an avalanche. You are separated from the whole set and are freezing. 
+        How will you find the set when going right looks the same as going left?`,
+
+        price: 19.99
+    },
+
+    {
+        name: 'Hiking Simulator: Tornado',
+
+        img: './pictures/tornado.jpg',
+
+        description: `The Government finally has control over the weather, and they decide to run a severe tornado in your small town.
+        You are consumed by the twister, thrown around like a leaf. You survive, what a miracle! But it appears that you nowhere near 
+        home. How will you get back home, or at least what's left of it. Hike your way home and tell everyone what happened.`,
+
+        price: 20.99
+    }, 
+
+    {
+        name: 'Hiking Simulator: Hurricane Makki',
+
+        img: './pictures/hurricane.jpg',
+
+        description: `The year is 2012 and the world is ending the way the Aztecs predicted. You are swept up in what became known as 
+        Hurricane Makki. After being thrashed around for twelve hours the notorious hurricane cools off. Can you hike it to the 
+        Ark before the next disaster comes along? Probably not, but you should try!`,
+        
+        price: 30.99
+    },
+
+    {
+        name: 'Hiking Simulator: Westworld',
+
+        img: './pictures/westworld.jpg',
+
+        description: `The robots from the Westworld amusement park have gained consciousness and have decided to eliminate all humans. 
+        Pretend to be a robot until you can find a way to the North or South Pole, where the robots cannot attack due to their machanical 
+        limitations.`,
+
+        price: 35.99
+    }
+]

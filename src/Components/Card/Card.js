bde943cb5a1d46f0c2aac300b2ed233c1d6604fb
Tyreeck M <mrmakki12@users.noreply.github.com>
2022-03-29 17:04:37 -0400

    card styles

--- src/Components/Card/Card.js
+++ src/Components/Card/Card.js
@@ -4,7 +4,7 @@ export const Card = ({data}) => {
     return (
         <div className="col" data-testid='card'>
             <div className="card text-white bg-dark">
-                <img className="card-img-top" src={data.img} />
+                <img className="card-img-top" src={data.img} alt={data.title}/>
                 <div className="card-body">
                     <h4 className="card-title">{data.title}</h4>
                     <p className="card-text">{data.description}</p>

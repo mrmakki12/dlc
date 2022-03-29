822231b1954595d5ac51c9983d3102f8142af9d5
Tyreeck M <mrmakki12@users.noreply.github.com>
2022-03-29 16:33:26 -0400

    styles

--- src/Components/DLC/DLC.js
+++ src/Components/DLC/DLC.js
@@ -1,23 +1,27 @@
 import React from "react";
 import { dlcs } from '../../static/dlc-data.js';
+// components 
+import { Card } from '../Card/Card.js';
 
 export const DLC = () => {
 
     return (
-        <div>
-            <div>
+        <div data-testid='dlc'>
+            <div className="container-fluid g-3 bg-dark text-white">
                 <h3>Best Sellers</h3>
             </div>
-            <div>
-                {
-                    dlcs.map((dlc, i) => {
-                        return (
-                            <div key={i}>
-                                {dlc.name}
-                            </div>
-                        )
-                    })
-                }
+            <div className="album bg-dark text-white ">
+                <div className="container">
+                    <div className="row rows-cols-1 row-cols-sm-2 row-cols-md-3 g-2">
+                        {
+                            dlcs.map((dlc, i) => {
+                                return (
+                                    <Card key={i} data={dlc} />
+                                )
+                            })
+                        }
+                    </div>
+                </div>
             </div>
         </div>
     )

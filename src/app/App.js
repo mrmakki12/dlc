bd6ee3ad837d9ba3a3cc24914a346e5c3fcd56d3
Tyreeck M <mrmakki12@users.noreply.github.com>
2022-03-29 14:57:55 -0400

    setup routes

--- src/app/App.js
+++ src/app/App.js
@@ -1,21 +1,21 @@
 import React from 'react';
 import './App.css';
 // router
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 // components
 import { Header } from '../Components/Header/Header.js';
 import { DLC } from '../Components/DLC/DLC';
+import { Cart } from '../Components/Cart/Cart';
 
 function App() {
   return (
     <div className="App">
-      <BrowserRouter>
+      <Router>
         <Routes>
-          
+          <Route exact path='/' element={<><Header /><DLC /></>} />
+          <Route exact path='/cart' element={<Cart />} />
         </Routes>
-        <Header />
-        <DLC />
-      </BrowserRouter>
+      </Router>
     </div>
   );
 }

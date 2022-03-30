720a19d48856d81e5ffecd4911be04a182b830c6
Tyreeck M <mrmakki12@users.noreply.github.com>
2022-03-30 14:53:40 -0400

    styles and test

--- src/Components/Card/Card.js
+++ src/Components/Card/Card.js
@@ -1,13 +1,13 @@
 import React from "react";
 
 export const Card = ({data}) => {
+
     return (
         <div className="col" data-testid='card'>
             <div className="card text-white bg-dark">
                 <img className="card-img-top" src={data.img} alt={data.title}/>
                 <div className="card-body">
-                    <h4 className="card-title">{data.title}</h4>
-                    <p className="card-text">{data.description}</p>
+                    <h4 className="card-title">{data.name}</h4>
                     <b><p className="card-text">${data.price}</p></b>
                 </div>
             </div>
--- src/Components/Card/card.test.js
+++ src/Components/Card/card.test.js
@@ -2,10 +2,39 @@ import React from "react";
 import { Card } from './Card.js';
 import { cleanup, render } from '@testing-library/react';
 import '@testing-library/jest-dom/extend-expect';
+import renderer from 'react-test-renderer';
+
 
 afterEach(cleanup);
 
 // basic render
 test("render Card without crashing", () => {
-    render(<Card />)
+    // sample data
+    const data = {
+        name: 'Hiking Simulator: Jungle Adventure',
+        img: '/pictures/background.jpg',
+        description: `Your on a mission to find the lost treasure of Atlantis with Nathan
+        Drake. You get separated from each during a confrontation with Sully and Marlow. Find your way
+        back!`,
+        price: `free 99`
+    }
+
+    render(<Card data={data}/>);
+});
+
+// snapshot
+test("matches snapshot", () => {
+    
+    // sample data
+    const data = {
+        name: 'Hiking Simulator: Jungle Adventure',
+        img: '/pictures/background.jpg',
+        description: `Your on a mission to find the lost treasure of Atlantis with Nathan
+        Drake. You get separated from each during a confrontation with Sully and Marlow. Find your way
+        back!`,
+        price: `free 99`
+    }
+
+    const tree = renderer.create(<Card data={data}/>).toJSON();
+    expect(tree).toMatchSnapshot();
 });

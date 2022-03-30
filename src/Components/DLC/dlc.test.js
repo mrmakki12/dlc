edee43d0591fc19ed639c3cc8b7d0e558126e2a2
Tyreeck M <mrmakki12@users.noreply.github.com>
2022-03-30 18:40:49 -0400

    added redux provider + store to test

--- src/Components/DLC/dlc.test.js
+++ src/Components/DLC/dlc.test.js
@@ -4,18 +4,22 @@ import { DLC } from './DLC.js';
 import { cleanup, render, screen } from '@testing-library/react';
 import '@testing-library/jest-dom/extend-expect';
 import renderer from 'react-test-renderer';
+// redux
+import { Provider } from 'react-redux';
+import { store } from '../../App/store.js';
 
 // cleanup
 afterEach(cleanup);
 
 // basic render 
 test('renders DLC without crashing', () => {
-    render(<DLC />);
+    render(<Provider store={store}><DLC /></Provider>);
 });
 
 // advanced render
 test('renders DLC completely', () => {
-    render(<DLC />);
+    render(<Provider store={store} ><DLC /></Provider>
+    );
 
     // best sellers header
     screen.getByText('Best Sellers');

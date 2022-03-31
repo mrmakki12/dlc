a1e4e14438b77a987f2b59cccc93dbe008941d25
Tyreeck M <mrmakki12@users.noreply.github.com>
2022-03-30 20:00:30 -0400

    and cart slice to state

--- src/app/store.js
+++ src/app/store.js
@@ -1,9 +1,10 @@
 import { configureStore } from '@reduxjs/toolkit';
 // reducers
-
+import cartSlice from '../Components/Cart/cartSlice';
 
 export const store = configureStore({
 
   reducer: {
+    cart: cartSlice
   },
 });

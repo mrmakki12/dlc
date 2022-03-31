ce33a9c6e550066c5c405fc9fc66598012badc89
Tyreeck M <mrmakki12@users.noreply.github.com>
2022-03-31 14:28:11 -0400

    styles

--- src/Components/Cart/Cart.js
+++ src/Components/Cart/Cart.js
@@ -2,6 +2,8 @@ import React from "react";
 // select cart data from store
 import { useSelector } from "react-redux";
 import { selectCart } from './cartSlice.js';
+// components
+import { CartItem } from '../CartItem/CartItem.js';
 
 export const Cart = () => {
 
@@ -9,7 +11,7 @@ export const Cart = () => {
     const cart = useSelector(selectCart);
 
     return (
-        <div data-testid='cart'> 
+        <div data-testid='cart' className='container-fluid text-white bg-dark'> 
             {
                 cart && cart.map(item => {
                     return (

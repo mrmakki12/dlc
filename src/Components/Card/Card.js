b3fcb4fa4ed1b01a8e2a4f73519a72075c757ca3
Tyreeck M <mrmakki12@users.noreply.github.com>
2022-03-30 20:01:06 -0400

    add to cart functionality

--- src/Components/Card/Card.js
+++ src/Components/Card/Card.js
@@ -1,25 +1,35 @@
 import React from "react";
 // redux
 import { useDispatch } from "react-redux";
+// add to cart
+import { addToCart } from "../Cart/cartSlice";
 
-export const Card = ({data}) => {
+export const Card = (props) => {
+
+    // data
+    const data = props.data;
+
 
     // add to cart
     const dispatch = useDispatch();
 
+    const handleAddToCart = (e) => {
+        e.preventDefault();
+        dispatch(addToCart(data));
+    }
+
 
     return (
-        <div className="col" data-testid='card' onClick={() => {}}>
+        <div className="col" data-testid='card'>
             <div className="card text-white bg-dark">
                 <img className="card-img-top" src={data.img} alt={data.title}/>
                 <div className="card-body">
                     <h4 className="card-title">{data.name}</h4>
                     <b><p className="card-text">${data.price}</p></b>
                 </div>
-                <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal">
-                    More info
+                <button type="button" id='atc' className="btn btn-primary" onClick={(e) => handleAddToCart(e)}>
+                    Add To Cart
                 </button>
-
             </div>
         </div>
     )

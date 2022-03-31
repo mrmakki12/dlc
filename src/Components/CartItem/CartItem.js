5e7e8c1e0f594367726ecce201724b1867879970
Tyreeck M <mrmakki12@users.noreply.github.com>
2022-03-31 14:48:09 -0400

    cart item structure done

--- src/Components/CartItem/CartItem.js
+++ src/Components/CartItem/CartItem.js
@@ -1,10 +1,38 @@
 import React from "react";
+import { useDispatch } from "react-redux";
+//remove / add to cart
+import { removeFromCart, addToCart } from "../Cart/cartSlice";
 
-export const CartItem = ({data}) => {
+export const CartItem = (props) => {
+
+    // data
+    const data = props.data;
+
+    // dispatch action to store
+    const dispatch = useDispatch();
+
+    // add another
+    const handleAddToCart = (e) => {
+        e.preventDefault();
+        dispatch(addToCart(data));
+    };
+
+    // remove item
+    const handleRemoveFromCart = (e) => {
+        e.preventDefault();
+        dispatch(removeFromCart(data));
+    }
+
     return (
         <div>
-            <div></div>
-            <div></div>
+            <div>
+                <h2>{data.name}</h2>
+                <p>Quantity: {data.quantity}</p>
+            </div>
+            <div>
+                <button onClick={(e) => handleRemoveFromCart(e)}>-</button>
+                <button onClick={(e) => handleAddToCart(e)}>+</button>
+            </div>
         </div>
     )
 }
